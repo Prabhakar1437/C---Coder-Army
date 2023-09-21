45d46c04aa58e64d1dbc9bc0558ca4fc07ca86fc
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-09-21 17:47:13 +0530

    Variabale and datatype

---
+++ Variable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // variable declartion
+    // int a;
+    // int a = 10, b = 20; // variable define
+    // cout << a + b;
+
+    // char c = 'a';
+    // cout << c;
+    float f = 2.31;
+    cout << f << endl;
+    double d = 2.3456;
+    cout << d << endl;
+
+    bool b = true;
+    cout << b << endl;
+}
