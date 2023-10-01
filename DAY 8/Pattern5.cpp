0db4a79d0b6ff13df7de052af22ac3b5e3fd7585
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-01 16:01:08 +0530

    DAY 8

---
+++ DAY 8/Pattern5.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int row = 1; row <= 5; row++)
+    {
+        char name = 'a' + row - 1;
+        for (int col = 1; col <= 5; col++)
+        {
+            cout << name << " ";
+        }
+        cout << endl;
+    }
+    // Print abcde
+    cout << "Print a to e" << endl;
+    for (int row = 1; row <= 5; row++)
+    {
+        for (char col = 'a'; col <= 'e'; col++)
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+}
