8340a2b4f3f7c6d5735eccf8d42b505cb0191536
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-02 11:47:06 +0530

    DAY 10

---
+++ DAY 10/Pattern17.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the input" << endl;
+    cin >> n;
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= n - row; col++)
+        {
+            cout << "  ";
+        }
+        for (int col = 1; col <= row; col++)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << endl;
+    }
+}
