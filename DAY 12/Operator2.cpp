9917915303381e2fa18772df59736f9daa6a066f
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-04 09:12:27 +0530

    DAY 12

---
+++ DAY 12/Operator2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Logical opeator
+    /*
+    int a, b, c;
+    cout << "Enter the number :";
+    cin >> a >> b >> c;
+    if (a > b && a > c)
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    */
+
+    // OR
+    /*
+    char name = 'i';
+    if (name == 'a' || name == 'e' || name == 'i' || name == 'o' || name == 'u')
+    {
+        cout << "Vowel";
+    }
+    else
+    {
+        cout << "Consonant";
+    }
+    */
+    // Not
+    cout << !23 << endl;
+    cout << !0;
+}
