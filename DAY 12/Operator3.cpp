25746af070f83c26d44a03487ad4f0ea0f3177d6
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-04 11:16:44 +0530

    DAY 12

---
+++ DAY 12/Operator3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Bitwise Operator
+    // AND
+    int ans = 2 & 3;
+    cout << ans << endl;
+    // OR
+    int a = 2 | 3;
+    cout << a << endl;
+    // XOR
+    int n = 2 ^ 3;
+    cout << n << endl;
+    // Left Shift
+    int s = 2 << 3;
+    cout << s << endl;
+    // Right Shift
+    int p = 18 >> 3;
+    cout << p << endl;
+    // compliment
+    int b = ~5;
+    cout << b << endl;
+}
