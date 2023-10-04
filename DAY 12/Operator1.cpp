dd582b8002a7e27a476ce5bf1f47b90a0f8d244e
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-04 09:12:00 +0530

    DAY 12

---
+++ DAY 12/Operator1.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Comparision Operator
+    int a, b;
+    cout << "Enter the value of a :";
+    cin >> a;
+    cout << "Enter the value of b :";
+    cin >> b;
+    /*
+    if (a == b)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    */
+    // Greater
+    /*
+    if (a > b)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    */
+    // Less than
+    /*
+    if (a < b)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    */
+    // greater than equal to
+    /*
+    if (a >= b)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    */
+    // Less than equal to
+    /*
+    if (a <= b)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+    */
+    // Not Equal to
+
+    if (a != b)
+    {
+        cout << "yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
