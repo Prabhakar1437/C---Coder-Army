0a2f9778374683d4ce727d3870765deb4696444f
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-05 21:19:01 +0530

    DAY 15

---
+++ DAY 15/gfgleapyear.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int isLeap(int N)
+    {
+        // code here
+        if (N % 400 == 0)
+        {
+            return 1;
+        }
+        else if (N % 4 == 0 && N % 100 != 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+};
