4ed0289384335a3784811077dd079f36ce3cf790
Prabhakar1437 <prabhakarvatsking@gmail.com>
2023-10-28 21:06:04 +0530

    DAY 22

---
+++ DAY 22/Array.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{   /*
+       int arr[5] = {2, 4, 7, 12, 6};
+  
+       for (int i = 0; i < 5; i++)
+       {
+           cout<<arr[i]<<" ";
+  
+  
+       }
+       */
+    /*
+      int arr[] = {2, 4, 7, 12, 6,8};
+
+     for (int i = 0; i < 6; i++)
+     {
+         cout<<arr[i]<<" ";
+
+
+     }
+     */
+    /*
+    int arr[5] = {
+2,
+        4,
+        7};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    */
+    /*
+     int arr[6];
+     for (int i = 0; i < 6; i++)
+
+         cin >> arr[i];
+
+     for (int i = 0; i < 6; i++)
+     {
+         cout << arr[i] << " ";
+     }
+     */
+    /*
+    int size;
+    cin >> size;
+    int arr[1000];
+    for (int i = 0; i < size; i++)
+
+        cin >> arr[i];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    */
+    int arr[5] = {1, 2, 3, 4, 5};
+    cout << sizeof(arr) / sizeof(arr[0]) << " ";
+}
